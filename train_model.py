23012551ac50a2c687b4925a8ff52110039100a7
Aditya Shome <shomeaditya65@gmail.com>
2025-07-24 16:07:48 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -15,5 +15,5 @@ model = Sequential([
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=2)
 
-model.save("model.keras")   # Recommended Keras format
+model.save("model.keras")
 
